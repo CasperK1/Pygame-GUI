3bf12d3b4d4824302227b89fd714521c89e810c8
CasperK1 <casper.kapiainen@metropolia.fi>
2024-03-01 23:36:54 +0200

    multiple changes

--- GUI.py
+++ GUI.py
@@ -1,9 +1,11 @@
 import pygame
 import random
-import main_menu, window_specs
+import window_specs
 
 pygame.init()
+pygame.mixer.init()
 window = window_specs.Window()
+
 
 def gui_loop():
 
@@ -13,7 +15,6 @@ def gui_loop():
     # Rendered layers.
     background = pygame.image.load('assets/bg32.png').convert_alpha()
     bg_scroll = 0
-    #font_surface = font.render('Flying to target...', True, 'lightslategrey').convert_alpha()
     player = pygame.image.load('assets/player_animation/player_1.png').convert_alpha()
     player_left1 = pygame.image.load('assets/player_animation/player_1_TURNL2.png').convert_alpha()
     player_left2 = pygame.image.load('assets/player_animation/player_1_TURNL.png').convert_alpha()
@@ -25,6 +26,7 @@ def gui_loop():
     turn_right_counter = 0
     player_coll = player.get_rect(topleft=(50, 150))
     HUD = pygame.image.load('assets/HUD.png').convert_alpha()
+    cockpit = pygame.image.load('assets/player_animation/LOCK WARNING.png').convert_alpha()
 
     # Radar animation
     radar_frames = [pygame.image.load(f'assets/radar/frame_91 ({i}).png') for i in range(1, 92)]
@@ -33,13 +35,36 @@ def gui_loop():
     # Clouds
     cloud_1 = pygame.image.load('assets/cloud1.png').convert_alpha()
     cloud_1_rect = cloud_1.get_rect(center=(1400, 100))
-
     cloud_2 = pygame.image.load('assets/cloud4.png').convert_alpha()
     cloud2_x_pos = 1200
 
-    # Game Loop
+    # Sounds
+    rwr_lock = pygame.mixer.Sound('assets/sounds/radar_lock.wav')
+    rwr_lock.set_volume(0.15)
+    rwr_warning = pygame.mixer.Sound('assets/sounds/rwr_WarningWarning.wav')
+    rwr_warning.set_volume(0.15)
+
+    last_check_time = pygame.time.get_ticks()
+    warning_screen_timer = 0
+    detect = False
     running = True
     while running:
+        # Check for enemies every 3 seconds (test)
+        current_time = pygame.time.get_ticks()
+        if (current_time - last_check_time) > 4000 and detect == False:  # 4000 milliseconds = 4 seconds
+            print('Checking for enemies...')
+            if random.random() < 0.3:  # 30% chance to counter enemy during trip
+                print('Enemy detected!')
+                rwr_warning.play()
+                rwr_lock.play(loops=-1)
+                detect = True
+                warning_screen_timer = current_time
+            last_check_time = current_time
+        if detect and  (current_time - warning_screen_timer) > 5000:
+            print('Starting combat...')
+            break
+
+
         keys = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
@@ -59,11 +84,11 @@ def gui_loop():
         if abs(bg_scroll) > background.get_width():
             bg_scroll = 0
         # Cloud 1
-        if cloud_1_rect.x < -300:
+        if cloud_1_rect.x < -1000:
             cloud_1_rect.x = 1200
             cloud_1_rect.y = random.randint(100, 150)
         window.screen.blit(cloud_1, cloud_1_rect)
-        cloud_1_rect.x -= 1.501
+        cloud_1_rect.x -= 2
         # player movement
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
@@ -99,9 +124,7 @@ def gui_loop():
         radar_frame_counter += 0.8
         if radar_frame_counter >= len(radar_frames):
             radar_frame_counter = 0
-
+        if detect == True:
+            window.screen.blit(cockpit, (0, 0))
         pygame.display.update()
         window.clock.tick(window.FPS)
-
-main_menu.main_menu()
-gui_loop()
--- main_menu.py
+++ main_menu.py
@@ -1,13 +1,21 @@
-import pygame, window_specs, pygame_gui
-import time
+import pygame
+import pygame_gui
 from pygame_gui.core import ObjectID
+import time
+import os
+import pickle
+import window_specs
+
+
+
 pygame.init()
 
 
 def main_menu():
     window = window_specs.Window()
     launch_button_manager = pygame_gui.UIManager((800, 600), 'assets/buttons/launch_button.json')
-    settings_manager = pygame_gui.UIManager((800, 600), 'assets/buttons/settings_button.json')
+    settings_manager = pygame_gui.UIManager((800, 600), 'assets/buttons/volume_button.json')
+    save_manager = pygame_gui.UIManager((800, 600), 'assets/buttons/save_button.json')
     # menu music
     pygame.mixer.init()
     pygame.mixer.music.load('assets/sounds/menu.mp3')
@@ -20,12 +28,18 @@ def main_menu():
     launch_b = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((450, 90), (250, 160)), text='',
                                             manager=launch_button_manager)
 
-    mute_b = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((20, 10), (44, 44)),
-                                              text='',
-                                              manager=settings_manager,
-                                              object_id=ObjectID(class_id='settings_button',
-                                                                object_id='mute'))
+    mute_b = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((20, 10), (66, 66)),
+                                          text='',
+                                          manager=settings_manager,
+                                          object_id=ObjectID(class_id='volume_button',
+                                                             object_id='mute'))
+    save_menu_b = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((20, 100), (66, 66)),
+                                               text='',
+                                               manager=save_manager,
+                                               object_id=ObjectID(class_id='save_button',
+                                                                  object_id='save'))
 
+    loaded_save = None
     main_running = True
     while main_running:
         time_delta = window.clock.tick(window.FPS) / 1000.0
@@ -33,30 +47,81 @@ def main_menu():
         for event in pygame.event.get():
             launch_button_manager.process_events(event)
             settings_manager.process_events(event)
+            save_manager.process_events(event)
             if event.type == pygame.QUIT or keys[pygame.K_ESCAPE]:
                 main_running = False
+
             if event.type == pygame_gui.UI_BUTTON_PRESSED:
-                if event.ui_element == launch_b:
+                if event.ui_element == launch_b:  # Launch button
                     button_click.play()
                     time.sleep(0.3)
-                    main_running = False
-                if event.ui_element == mute_b:
+                    if loaded_save == None:
+                        launch_b.hide()
+                        name_input = pygame_gui.elements.UITextEntryLine(
+                            relative_rect=pygame.Rect((430, 180), (310, 70)),
+                            manager=launch_button_manager,
+                            object_id=pygame_gui.core.ObjectID(class_id='name_input', object_id='UITextEntryLine'),
+                            placeholder_text='No save file. Enter your name: '
+                        )
+                    else:
+                        return loaded_save
+
+                if event.ui_element == mute_b:  # Mute button
                     if pygame.mixer.music.get_volume() == 0:
                         pygame.mixer.music.set_volume(0.1)
                         button_click.play()
                     else:
                         pygame.mixer.music.set_volume(0)
                         button_click.play()
-                        
+                if event.ui_element == save_menu_b:  # Save button
+                    button_click.play()
+                    save_window = pygame_gui.windows.UIFileDialog(rect=pygame.Rect((20, 100), (260, 300)),
+                                                                  manager=save_manager,
+                                                                  window_title='LOAD GAME',
+                                                                  initial_file_path='savedata',
+                                                                  allowed_suffixes='.sav',
+                                                                  allow_picking_directories=False,
+                                                                  allow_existing_files_only=True)
+                    save_window.show()
+
+                if event.type == pygame.USEREVENT and event.user_type == pygame_gui.UI_WINDOW_CLOSE:
+                    if event.ui_object_id == '#save_window':  # Check if the closed window is the save window
+                        save_menu_b.show()  # Show the save button again
+
+
+            if event.type == pygame_gui.UI_FILE_DIALOG_PATH_PICKED:  # Load game window
+                if event.ui_element == save_window:
+                    save_path = event.text
+                    filename = os.path.basename(save_path)
+                    file_path = os.path.join('savedata', filename)
+                    if os.path.exists(file_path):
+                        with open(os.path.join('savedata', filename), 'rb') as output:
+                            print(f'\nLoaded {filename}')
+                            loaded_save = pickle.load(output)
+                            print(f'Save file player name: {loaded_save[0].player_name}')
+                            print(f'Save file player level: {loaded_save[0].player_level}')
+                    else:
+                        print("File not found/ File error")
+
+            if event.type == pygame_gui.UI_TEXT_ENTRY_FINISHED:
+                player_name = event.text
+
+                print(f'\nNew game started as {player_name}\n')
+                return player_name
+
+
+
         settings_manager.update(time_delta)
         launch_button_manager.update(time_delta)
+        save_manager.update(time_delta)
 
         window.screen.blit(menu_background, (-30, 0))
         launch_button_manager.draw_ui(window.screen)
         settings_manager.draw_ui(window.screen)
+        save_manager.draw_ui(window.screen)
 
         pygame.display.update()
         window.clock.tick(window.FPS)
 
 
-main_menu()
+
